e4b5b99ee83dd556bf3228d278a584fea93bb81b
MARCOS-S-S <marcossilqueira565@gmail.com>
2025-11-25 09:39:12 -0300

    New version 1.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.mss.thebigcalendar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.7.0"
+        versionCode = 13
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
