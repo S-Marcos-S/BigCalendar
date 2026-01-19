0059f71a34ba78d803316539fa32d62eb764135e
MARCOS-S-S <marcossilqueira565@gmail.com>
2026-01-19 12:18:59 -0300

    Removing unnecessary permission.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.mss.thebigcalendar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "1.7.1"
+        versionCode = 25
+        versionName = "1.7.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
