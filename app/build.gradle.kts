72eb78194e40d4edffbf0be993bb770e0e6cc5c6
MARCOS-S-S <marcossilqueira565@gmail.com>
2026-06-08 21:12:49 -0300

    Bump version to 1.7.99 (Code: 44)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.mss.thebigcalendar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 43
+        versionCode = 44
         versionName = "1.7.99"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
