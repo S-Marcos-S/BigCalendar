befbefb49c46d8d0d8a134a07a505b213d24771b
MARCOS-S-S <marcossilqueira565@gmail.com>
2026-06-08 21:50:18 -0300

    Bump version to 1.8.0 (Code: 45)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.mss.thebigcalendar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 44
-        versionName = "1.7.99"
+        versionCode = 45
+        versionName = "1.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
