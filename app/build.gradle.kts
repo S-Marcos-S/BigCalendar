51c2b39d08a718028b88b2bf440333347f93b8c5
S_Marcos_S <marcossilqueira565@gmail.com>
2026-08-28 11:06:24 -0300

    ci: safe keystore properties generation and explicit app assembleRelease

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,6 +89,11 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1,DEPENDENCIES}"
         }
     }
+
+    lint {
+        abortOnError = false
+        checkReleaseBuilds = false
+    }
 }
 
 protobuf {
