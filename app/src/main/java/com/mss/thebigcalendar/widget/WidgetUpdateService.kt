b0d3cb026ff5e718877e74304368dc27858889d3
S_Marcos_S <marcossilqueira565@gmail.com>
2026-08-28 14:44:40 -0300

    fix(widget): adicionar import de R ausente em WidgetUpdateService

--- app/src/main/java/com/mss/thebigcalendar/widget/WidgetUpdateService.kt
+++ app/src/main/java/com/mss/thebigcalendar/widget/WidgetUpdateService.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import com.mss.thebigcalendar.R
 import kotlinx.coroutines.*
 import java.time.LocalDate
 import java.time.LocalTime
