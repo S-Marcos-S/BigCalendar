734d9ba60eab7f21029b3d06baeba25cd55e567c
S_Marcos_S <marcossilqueira565@gmail.com>
2026-08-28 10:11:35 -0300

    chore(release): bump version to v1.8.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.mss.thebigcalendar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 52
-        versionName = "1.8.7"
+        versionCode = 53
+        versionName = "1.8.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
