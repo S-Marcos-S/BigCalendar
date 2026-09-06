8e4bbba2c16088ae1b3082d43b591ecfbf036925
S_Marcos_S <marcossilqueira565@gmail.com>
2026-09-06 15:02:21 -0300

    Revert "Configure Termux Android build environment"
    This reverts commit 501dfd8354611dcc0f1dc3ba266b483c2fe8f056.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,12 +35,16 @@ android {
     }
 
     // Adicione este bloco para carregar as propriedades da sua keystore
+<<<<<<< HEAD
     val keystorePropertiesFile = when {
         rootProject.file("keystore.properties").exists() -> rootProject.file("keystore.properties")
         file("keystore.properties").exists() -> file("keystore.properties")
         rootProject.file("app/keystore.properties").exists() -> rootProject.file("app/keystore.properties")
         else -> rootProject.file("keystore.properties")
     }
+=======
+    val keystorePropertiesFile = rootProject.file("app/keystore.properties")
+>>>>>>> parent of 501dfd8 (Configure Termux Android build environment)
     val keystoreProperties = Properties()
     if (keystorePropertiesFile.exists()) {
         keystoreProperties.load(keystorePropertiesFile.inputStream())
@@ -116,11 +120,10 @@ protobuf {
     protoc {
         artifact = libs.protobuf.protoc.get().toString()
     }
-
     generateProtoTasks {
         all().forEach { task ->
-            task.builtins {
-                findByName("java")?.option("lite") ?: create("java") {
+            task.plugins {
+                create("java") {
                     option("lite")
                 }
             }
